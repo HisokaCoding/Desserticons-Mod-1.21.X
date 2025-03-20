aa01bbb4b090d117c4fa551fbd3211de73964b9f
HisokaCoding <zarniacci337@gmail.com>
2025-03-20 19:17:18 +0500

    Template

---
+++ src/main/java/net/hisoka/desserticonsmod/DesserticonsMod.java
@@ -0,0 +1,16 @@
+package net.hisoka.desserticonsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DesserticonsMod implements ModInitializer {
+	public static final String MOD_ID = "desserticonsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/hisoka/desserticonsmod/DesserticonsModClient.java
@@ -0,0 +1,10 @@
+package net.hisoka.desserticonsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DesserticonsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

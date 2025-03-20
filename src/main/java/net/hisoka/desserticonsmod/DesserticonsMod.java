688b66db072dd93fb0ff0e4b475f1065fc39c843
HisokaCoding <zarniacci337@gmail.com>
2025-03-20 19:57:08 +0500

    Desserticoin added

--- src/main/java/net/hisoka/desserticonsmod/DesserticonsMod.java
+++ src/main/java/net/hisoka/desserticonsmod/DesserticonsMod.java
@@ -2,6 +2,7 @@ package net.hisoka.desserticonsmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.hisoka.desserticonsmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class DesserticonsMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }

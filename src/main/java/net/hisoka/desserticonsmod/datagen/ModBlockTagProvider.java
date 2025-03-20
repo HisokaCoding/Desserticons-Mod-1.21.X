03a5dd980818e30fb4f545e28075061217861731
HisokaCoding <zarniacci337@gmail.com>
2025-03-21 02:04:35 +0500

    Datagen added

---
+++ src/main/java/net/hisoka/desserticonsmod/datagen/ModBlockTagProvider.java
@@ -0,0 +1,24 @@
+package net.hisoka.desserticonsmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.hisoka.desserticonsmod.block.ModBlocks;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+    public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(ModBlocks.DESSERTICOIN_BLOCK);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.DESSERTICOIN_BLOCK);
+    }
+}
---
+++ src/main/java/net/hisoka/desserticonsmod/datagen/ModRecipeProvider.java
@@ -0,0 +1,40 @@
+package net.hisoka.desserticonsmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.hisoka.desserticonsmod.DesserticonsMod;
+import net.hisoka.desserticonsmod.block.ModBlocks;
+import net.hisoka.desserticonsmod.item.ModItems;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter recipeExporter) {
+
+        offerReversibleCompactingRecipes(recipeExporter, RecipeCategory.BUILDING_BLOCKS, ModItems.DESSERTICOIN, RecipeCategory.MISC, ModBlocks.DESSERTICOIN_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.DESSERTICOIN)
+                .pattern("##")
+                .pattern("##")
+                .input('#', Items.GOLD_NUGGET)
+                .criterion(hasItem(Items.GOLD_NUGGET), conditionsFromItem(Items.GOLD_NUGGET))
+                .offerTo(recipeExporter,Identifier.of(DesserticonsMod.MOD_ID, "desserticoin_from_nuggets"));
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.DESSERTICOIN, 9)
+                .input(ModBlocks.DESSERTICOIN_BLOCK)
+                .criterion(hasItem(ModBlocks.DESSERTICOIN_BLOCK),conditionsFromItem(ModBlocks.DESSERTICOIN_BLOCK))
+                .offerTo(recipeExporter, Identifier.of(DesserticonsMod.MOD_ID, "desserticoin_from_block"));
+    }
+}

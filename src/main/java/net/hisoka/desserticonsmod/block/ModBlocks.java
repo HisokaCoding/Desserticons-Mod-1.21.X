b7467a4c671945700bd53f4a68f2bbbcf6ae222f
HisokaCoding <zarniacci337@gmail.com>
2025-03-21 00:54:58 +0500

    Loot tables added

--- src/main/java/net/hisoka/desserticonsmod/block/ModBlocks.java
+++ src/main/java/net/hisoka/desserticonsmod/block/ModBlocks.java
@@ -1,12 +1,10 @@
 package net.hisoka.desserticonsmod.block;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.hisoka.desserticonsmod.DesserticonsMod;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
